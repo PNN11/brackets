0e13174feb289f2a241078df25e5f4131d2dac31
PNN11 <47753604+PNN11@users.noreply.github.com>
2019-03-24 20:47:42 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let openBracket = [];
+    let closeBracket = [];
+    let result = [];
+
+    for (i = 0; i < bracketsConfig.length; i++) {
+        openBracket.push(bracketsConfig[i][0]);
+        closeBracket.push(bracketsConfig[i][1]);
+    }
+
+    for (i = 0; i < str.length; i++) {
+        let bracket = str[i];
+        if (openBracket.indexOf(bracket) !== -1) {
+            if (result[result.length - 1] !== closeBracket.indexOf(bracket)) {
+                result.push(openBracket.indexOf(bracket));
+                continue
+            }
+        }
+
+        if (closeBracket.indexOf(bracket) !== -1) {
+            let last = result.pop();
+            if (closeBracket.indexOf(bracket) !== last) {
+                return false
+            }
+        }
+    }
+
+    return result.length === 0
 }
